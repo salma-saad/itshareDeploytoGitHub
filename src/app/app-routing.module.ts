3abe2e20803147a78d75c650988b1ee899ce7d65
salma-saad <salmasaad94@gmail.com>
2019-03-24 15:48:09 +0200

    all source code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+const routes: Routes = [
+  {path: 'shopping-cart' , component:ShoppingCartComponent},
+  {path: 'products' , component:ProductsComponent},
+  {path: 'orders' , component:MyOrdersComponent},
+  {path: 'admin/orders' , component:AdminOrdersComponent},
+  {path: 'admin/products' , component:AdminProductsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
